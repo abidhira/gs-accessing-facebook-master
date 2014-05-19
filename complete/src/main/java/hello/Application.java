25f4f305bd0cd15dcd9759d8ff9830f200ecdf59
abidhira <born2india@gmail.com>
2014-05-20 03:57:29 +0530

    a
    a

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.social.UserIdSource;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
+
+@Configuration
+@EnableAutoConfiguration
+@Import(FacebookConfig.class)
+@ComponentScan
+public class Application {
+
+    /*
+     * SPRING BOOTSTRAP MAIN
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+} 
---
+++ complete/src/main/java/hello/FacebookConfig.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.social.UserIdSource;
+import org.springframework.social.config.annotation.EnableInMemoryConnectionRepository;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.facebook.config.annotation.EnableFacebook;
+
+@EnableFacebook(appId="270355659810630", appSecret="39a6ba6e98261defa15ea31b127c3fe7")
+@EnableInMemoryConnectionRepository
+public class FacebookConfig {
+
+    @Bean
+    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
+        return new ConnectController(connectionFactoryLocator, connectionRepository);
+    }
+
+    @Bean
+    public UserIdSource userIdSource() {
+        return new UserIdSource() {
+            @Override
+            public String getUserId() {
+                return "testuser";
+            }
+        };
+    }
+
+}
